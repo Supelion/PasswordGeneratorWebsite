b185d53d6950bc6c8a7b67e742452253c3391c30
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2021-10-16 11:22:25 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const lettersAndOtherThings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', "A", "B", "C", "D", 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', "W", 'X', 'Y', 'Z'];
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', "A", "B", "C", "D", 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', "W", 'X', 'Y', 'Z'];
 const numbers = ["1", "2", "3", "4", "5", "6", "8", "8", "9", "0"];
 const symbols = ["!", "@", "#", "$"];
 
@@ -27,22 +27,22 @@ function getRandomPassword() {
         char13Random = Math.floor(Math.random() * symbols.length);
     };
 
-    const char1 = lettersAndOtherThings[char1Random];
-    const char2 = lettersAndOtherThings[char2Random];
-    const char3 = lettersAndOtherThings[char3Random];
-    const char4 = lettersAndOtherThings[char4Random];
+    const char1 = letters[char1Random];
+    const char2 = letters[char2Random];
+    const char3 = letters[char3Random];
+    const char4 = letters[char4Random];
     const char5 = numbers[char5Random];
-    const char6 = lettersAndOtherThings[char6Random];
-    const char7 = lettersAndOtherThings[char7Random];
-    const char8 = lettersAndOtherThings[char8Random];
-    const char9 = lettersAndOtherThings[char9Random];
+    const char6 = letters[char6Random];
+    const char7 = letters[char7Random];
+    const char8 = letters[char8Random];
+    const char9 = letters[char9Random];
     const char10 = numbers[char10Random];
     const char11 = numbers[char11Random];
     const char12 = numbers[char12Random];
     const char13 = symbols[char13Random];
     const char14 = symbols[char14Random];
 
-    const chosenPassword = char1 + char2 + char3 + char4 + char5 + char6 + char7 + char8 + char9 + char10 + char11 + char12 + char13 + char14;
+    const chosenPassword = char1 +  + char13 + char2 + char3 + char14 + char4 + char5 + char6 + char7 + char8 + char9 + char10 + char11 + char12;
 
     passwordDisplayElement.innerHTML = 'Your generated password is: <code id = "password-out">' + chosenPassword + "</code>";
 };
