6bdd145004b0732e9c086bb0ec2cc0870bd3595f
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2021-10-16 11:39:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,41 +8,37 @@ const generatePasswordButton = document.querySelector("#password-button");
 generatePasswordButton.addEventListener("click", getRandomPassword);
 
 function getRandomPassword() {
-    const char1Random = Math.floor(Math.random() * letters.length);
-    const char2Random = Math.floor(Math.random() * letters.length);
-    const char3Random = Math.floor(Math.random() * letters.length);
-    const char4Random = Math.floor(Math.random() * letters.length);
-    const char5Random = Math.floor(Math.random() * numbers.length);
-    const char6Random = Math.floor(Math.random() * letters.length);
-    const char7Random = Math.floor(Math.random() * letters.length);
-    const char8Random = Math.floor(Math.random() * letters.length);
-    const char9Random = Math.floor(Math.random() * letters.length);
-    const char10Random = Math.floor(Math.random() * numbers.length);
-    const char11Random = Math.floor(Math.random() * numbers.length);
-    const char12Random = Math.floor(Math.random() * numbers.length);
-    const char13Random = Math.floor(Math.random() * symbols.length);
-    const char14Random = Math.floor(Math.random() * symbols.length);
+    const char1RandomIndex = Math.floor(Math.random() * letters.length);
+    const char2RandomIndex = Math.floor(Math.random() * letters.length);
+    const char3RandomIndex = Math.floor(Math.random() * letters.length);
+    const char4RandomIndex = Math.floor(Math.random() * letters.length);
+    const char5RandomIndex = Math.floor(Math.random() * letters.length);
+    const char6RandomIndex = Math.floor(Math.random() * letters.length);
+    const char7RandomIndex = Math.floor(Math.random() * letters.length);
+    const char8RandomIndex = Math.floor(Math.random() * letters.length);
+    const char9RandomIndex = Math.floor(Math.random() * numbers.length);
+    const char10RandomIndex = Math.floor(Math.random() * numbers.length);
+    const char11RandomIndex = Math.floor(Math.random() * numbers.length);
+    const char12RandomIndex = Math.floor(Math.random() * numbers.length);
+    const char13RandomIndex = Math.floor(Math.random() * symbols.length);
+    const char14RandomIndex = Math.floor(Math.random() * symbols.length);
+    
+    const letter1 = letters[char1RandomIndex];
+    const letter2 = letters[char2RandomIndex];
+    const letter3 = letters[char3RandomIndex];
+    const letter4 = letters[char4RandomIndex];
+    const letter5 = letters[char5RandomIndex];
+    const letter6 = letters[char6RandomIndex];
+    const letter7 = letters[char7RandomIndex];
+    const letter8 = letters[char8RandomIndex];
+    const number1 = numbers[char9RandomIndex];
+    const number2 = numbers[char10RandomIndex];
+    const number3 = numbers[char11RandomIndex];
+    const number4 = numbers[char12RandomIndex];
+    const symbol1 = symbols[char13RandomIndex];
+    const symbol2 = symbols[char14RandomIndex];
 
-    if (char13Random == char14Random) {
-        char13Random = Math.floor(Math.random() * symbols.length);
-    };
-
-    const char1 = letters[char1Random];
-    const char2 = letters[char2Random];
-    const char3 = letters[char3Random];
-    const char4 = letters[char4Random];
-    const char5 = numbers[char5Random];
-    const char6 = letters[char6Random];
-    const char7 = letters[char7Random];
-    const char8 = letters[char8Random];
-    const char9 = letters[char9Random];
-    const char10 = numbers[char10Random];
-    const char11 = numbers[char11Random];
-    const char12 = numbers[char12Random];
-    const char13 = symbols[char13Random];
-    const char14 = symbols[char14Random];
-
-    const chosenPassword = char1 +  + char13 + char2 + char3 + char14 + char4 + char5 + char6 + char7 + char8 + char9 + char10 + char11 + char12;
+    const chosenPassword = letter1 + letter2 + symbol1 + letter3 + number1 + symbol2 + letter4 + letter5 + number2 + number3 + letter6 + number4 + letter7 + letter8;
 
     passwordDisplayElement.innerHTML = 'Your generated password is: <code id = "password-out">' + chosenPassword + "</code>";
 };
